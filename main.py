5a785ead2f6b54ee16fd2809706022c33ebe9dbd
Brendan Burns <bburns@microsoft.com>
2022-12-09 23:27:41 +0000

    Update python example

--- main.py
+++ main.py
@@ -4,6 +4,30 @@ class PythonWasm:
     
     def print(self):
         print(self.message)
+    
+    def write(self, filename, message):
+        file = open(filename, "w")
+        file.write(message)
+        file.close()
+    
+    def copy(self, fromFile, toFile):
+        f1 = open(fromFile, "r")
+        f2 = open(toFile, "w")
+
+        for line in f1.readlines():
+            f2.write(f"{line}\n")
+        
+        f1.close()
+        f2.close()
 
 wasm = PythonWasm()
 wasm.print()
+
+# Python + wasm is a little odd in how it expects the filesystem to be
+# laid out, so we place this file in the /workspace/ directory in the
+# wasm environment
+wasm.write("/workspace/test.txt", "this is a test")
+print("Wrote file")
+
+wasm.copy("/workspace/test.txt", "/workspace/test-2.txt")
+print("Copied file")
